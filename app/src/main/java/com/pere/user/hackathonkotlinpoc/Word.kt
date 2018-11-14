63bff787d3ac8cc16cc37791c58e2f0d9f5d6d47
Yoav Gross <yogross@gmail.com>
2018-11-14 23:20:02 +0200

    dependencies

---
+++ app/src/main/java/com/pere/user/hackathonkotlinpoc/Word.kt
@@ -0,0 +1,12 @@
+package com.pere.user.hackathonkotlinpoc
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Created by Yoav G on 14/11/2018.
+ */
+
+@Entity(tableName = "words")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word : String)
